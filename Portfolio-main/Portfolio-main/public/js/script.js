e428f5549ce28ee32c29b0b09e633631cd885bef
rakasyaaahh <farrelrakasyah@gmail.com>
2024-10-19 01:57:18 +0700

    Add files via upload

---
+++ Portfolio-main/Portfolio-main/public/js/script.js
@@ -0,0 +1,69 @@
+// Navbar Fixed 
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#toTop');
+
+    if( window.scrollY > fixedNav ) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+    } else {
+        toTop.classList.add('hidden');
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    navMenu.classList.toggle('hidden')
+})
+
+window.addEventListener('click', function(e) {
+    if( e.target != hamburger && e.target != navMenu ) {
+        hamburger.classList.remove('navbar-fixed')   
+        navMenu.classList.add('hidden')
+    }
+})
+
+// Theme
+const sun = document.querySelector('#sun');
+const moon = document.querySelector('#moon');
+const html = document.querySelector('html')
+
+sun.addEventListener('click', function() {
+    sun.classList.add('hidden');
+    moon.classList.remove('hidden')
+    html.classList.add('dark')
+})
+
+moon.addEventListener('click', function() {
+    moon.classList.add('hidden')
+    sun.classList.remove('hidden')
+    html.classList.remove('dark')
+})
+
+const sunSM = document.querySelector('#sunSM');
+const moonSM = document.querySelector('#moonSM');
+
+sunSM.addEventListener('click', function() {
+    sunSM.classList.add('hidden');
+    moonSM.classList.remove('hidden')
+    html.classList.add('dark')
+})
+
+moonSM.addEventListener('click', function() {
+    moonSM.classList.add('hidden')
+    sunSM.classList.remove('hidden')
+    html.classList.remove('dark')
+})
+
+// Reveal Text
+const text = baffle('#data');
+text.set({
+    characters: '<░████<▒█▓>░/<▓▓ ▓▓▓▓/▒█▒░▒<█▒▓▓░▓/█▒',
+    speed: 50
+});
+text.start();
+text.reveal(2000);
---
+++ Portfolio-main/Portfolio-main/tailwind.config.js
@@ -0,0 +1,98 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '40px',
+    },
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        dark: '#141414',
+        secondary: '#64748b',
+        html: '#E44D26',
+        css: '#1572B6',
+        javascript: '#F0DB4F',
+        bootstrap: '#7952B3',
+        github: '#000000',
+        jquery: '#78CFF5'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-45deg)' },
+          '50%': { transform: 'rotate(45deg)' }
+        },
+        fadeRight: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-2rem)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)'
+          }
+        },
+        fadeLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(2rem)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)'
+          }
+        },
+        fadeUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(2rem)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        },
+        wiggleUp: {
+          '0%, 100%': {
+            transform: 'translateY(0)'
+          },
+          '50%': {
+            transform: 'translateY(-3%)'
+          },
+        },
+        wiggleUpFast: {
+          '0%, 100%': {
+            transform: 'translateY(0)'
+          },
+          '50%': {
+            transform: 'translateY(-40%)'
+          },
+        }
+      },
+      animation: {
+        'wiggle-1.5s': 'wiggle 1500ms ease-in-out infinite',
+        'wiggle-2s': 'wiggle 2000ms ease-in-out infinite',
+        'wiggle-2.5s': 'wiggle 2500ms ease-in-out infinite',
+        'fadeRight-500': 'fadeRight 400ms ease-in-out',
+        'fadeRight-800': 'fadeRight 800ms ease-in-out',
+        'fadeRight-1.1': 'fadeRight 1100ms ease-in-out',
+        'fadeRight-1.5': 'fadeRight 1500ms ease-in-out',
+        'fadeLeft-500': 'fadeLeft 500ms ease-in-out',
+        'fadeLeft-800': 'fadeLeft 800ms ease-in-out',
+        'fadeLeft-1.1': 'fadeLeft 1100ms ease-in-out',
+        'fadeLeft-1.5': 'fadeLeft 1500ms ease-in-out',
+        'fadeUp': 'fadeUp 1000ms ease-in-out',
+        'bounce': 'bounce 2000ms ease-in-out infinite',
+        'wiggleUp': 'wiggleUp 4000ms ease-in-out infinite',
+        'wiggleUpFast': 'wiggleUp 1000ms ease-in-out infinite'
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animated'),
+    require('tailwind-scrollbar')
+  ],
+}
+
